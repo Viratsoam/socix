f2a4badf95431fc3e3c12f6e6c39cada7fd5726a
Viratsoam <viratsoam1105>
2020-07-04 16:09:21 +0530

    Server Started

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+// website run on the port number 80, in production
+const PORT = 8000;
+
+
+
+
+
+
+// Start the server
+app.listen(PORT,(err)=>{
+    if(err){
+        Console.log(`Error while Starting the Server:${err}`);
+        return;
+    }
+    console.log(`Server is up on PORT:${PORT}`);
+});
